46e6bf4b6877e1ee1ee1705714c4a292a250f09a
Darius Calliet <calliet.d@gmail.com>
2018-10-13 18:09:01 -0500

    more verbose language

--- __init__.py
+++ __init__.py
@@ -4,35 +4,52 @@ from mycroft.util.log import getLogger
 from mycroft.audio import wait_while_speaking
 from mycroft.skills.context import *
 
+import time
 
 class SnapResourceSkill(MycroftSkill):
     '''
         This skill has a dialogue for determining snap eligibility.
+
+        Where can I go for more snap info?
+
+        Am I eligible for food stamps?
     '''
+
+
     def __init__(self):
         MycroftSkill.__init__(self)
 
     @intent_file_handler('snap.eligibility.intent')
     def handle_resources_snap(self, message):
+        is_yes = lambda x: (x == "yes")
+        is_no = lambda x: (x == "no")
         self.speak_dialog('snap.eligibility')
         wait_while_speaking()
+
+        time.sleep(5)
         eligibility_info = self.ask_yesno('snap.more.info')
         wait_while_speaking()
-        if eligibility_info == "yes":
+
+        if is_yes(eligibility_info):
+            self.record_info = self.ask_yesno('collect.more.info')
+
+            if is_yes(self.record_info):
+                self.phone_number = self.get_response('ask.phone.number')
+
             self.citizen = self.ask_yesno('citizen')
             wait_while_speaking()
-            if not self.citizen:
+            if is_no(self.citizen):
                 self.five_years = self.ask_yesno('five.years')
                 wait_while_speaking()
 
-            if self.citizen or self.five_years:
+            if is_yes(self.citizen) or is_yes(self.five_years):
                 self.employment = self.ask_yesno('employment')
                 wait_while_speaking()
-                if self.employment:
+                if is_yes(self.employment):
                     self.income = self.ask_yesno('income')
                     wait_while_speaking()
 
-                    if self.income:
+                    if is_yes(self.income):
                         self.speak_dialog('generic.yes')
                     else:
                         self.speak_dialog('generic.no')
