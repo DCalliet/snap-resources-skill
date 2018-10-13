4c9a50a194d70a1e9343bc5cbec70b966e798378
Darius Calliet <calliet.d@gmail.com>
2018-10-13 16:04:19 -0500

    handle resources

--- __init__.py
+++ __init__.py
@@ -6,13 +6,39 @@ from mycroft.skills.context import *
 
 
 class SnapResources(MycroftSkill):
+    '''
+        This skill has a dialogue for determining snap eligibility.
+    '''
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('')
-    def handle_resources_citizenship(self, message):
-        pass
+    @intent_file_handler('snap.eligibility.intent')
+    def handle_resources_snap(self, message):
+        self.speak_dialogue('snap.eligibility')
+        wait_while_speaking()
+        eligibility_info = self.ask_yesno('snap.more.info')
+        wait_while_speaking()
+        if eligibility_info == "yes":
+            self.citizen = self.ask_yesno('citizen')
+            wait_while_speaking()
+            if not self.citizen:
+                self.five_years = self.ask_yesno('five.years')
+                wait_while_speaking()
 
+            if self.citizen or self.five_years:
+                self.employment = self.ask_yesno('employment')
+                wait_while_speaking()
+                if self.employment:
+                    self.income = self.ask_yesno('income')
+                    wait_while_speaking()
+
+                    if self.income:
+                        self.speak_dialogue('generic.yes')
+                    else:
+                        self.speak_dialogue('generic.no')
+        else:
+            self.speak_dialogue('here.to.assist')
+            wait_while_speaking()
 
 def create_skill():
-    return CitizenshipResources()
+    return SnapResources()
